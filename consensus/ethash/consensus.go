ddb4772b7fcc0af8541231c6e13be823db5d739d
Hannes <hannes@guedelhoefer.de>
2018-01-25 15:33:25 +0100

    Set difiiculty to 1

--- consensus/ethash/consensus.go
+++ consensus/ethash/consensus.go
@@ -295,7 +295,7 @@ func (ethash *Ethash) CalcDifficulty(chain consensus.ChainReader, time uint64, p
 // the difficulty that a new block should have when created at time
 // given the parent block's time and difficulty.
 func CalcDifficulty(config *params.ChainConfig, time uint64, parent *types.Header) *big.Int {
-	return new(big.Int).SetUint64(200);
+	return new(big.Int).SetUint64(1);
 	next := new(big.Int).Add(parent.Number, big1)
 	switch {
 	case config.IsByzantium(next):
